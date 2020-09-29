0cf97ad21b42736bff856a3ef45085c54fae4464
Aishwarya Mustoori <59821571+aishwarya-mustoori@users.noreply.github.com>
2020-09-29 11:15:38 -0700

    Add files via upload

---
+++ Data Collection/babari-parser.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+import requests
+from argparse import ArgumentParser
+from database import ReplayDatabase
+import time
+from selenium import webdriver 
+
+# Taken from https://github.com/vasumv/pokemon_ai/tree/master/log_scraper
+
+BABIRI_URL = "https://www.babiri.net/#/"
+REPLAY_URL= "http://replay.pokemonshowdown.com/{replay_id}.log"
+
+def parse_args():
+    argparser = ArgumentParser()
+    argparser.add_argument('db_path')
+    return argparser.parse_args()
+
+def get_teams():
+    text = requests.get(BABIRI_URL)
+    driver = webdriver.Chrome("/Users/aishwaryamustoori/Downloads/chromedriver")
+    driver.get(BABIRI_URL)
+    time.sleep(2)
+    html = driver.page_source
+    soup = BeautifulSoup(html,'html.parser')
+    links = soup.find_all('div',{"class":"card-body"})
+    all_teams = []
+   
+    for link in links : 
+        team_list = list()
+        images = link.find_all('img')
+        for image in images:
+            team_list.append(image.get('alt'))
+        replay_id = link.find_all('a',{"class","card-link"})
+        href = ""
+        for replay in replay_id : 
+            href = replay.get("href").strip("https://replay.pokemonshowdown.com/")
+        if(len(team_list)==6) : 
+            all_teams.append([team_list,href])
+
+    return all_teams
+
+
+def get_logs(replay_id):
+    html = requests.get(REPLAY_URL.format(
+        replay_id=replay_id)
+    ).text
+    return html
+
+if __name__ == "__main__":
+    args = parse_args()
+    teams = get_teams() 
+    r = ReplayDatabase(args.db_path)
+    for team in teams:
+        print ("Team: %s" % team)
+        replay_id = team[1]
+        if r.check_team_replay_exists(team[1]):
+            print ("Skipped Team: %s" % str(team[0]))
+            continue
+        print ("New replay ID: %s" % replay_id)
+        r.add_team_replay(str(team[0]),replay_id, get_logs(replay_id))
+        r.commit()
